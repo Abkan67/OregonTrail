8d651f3e39b3a0023cedc821ba5152869534b2bd
Abkan67 <78571678+Abkan67@users.noreply.github.com>
2021-06-27 21:02:55 -0400

    Delete wagonband.js

--- wagonband.js
+++
@@ -1,9 +0,0 @@
-class WagonBand {
-  constructor() {
-    this.crew = [];
-    this.money = 5000;// TODO: Change money here
-    this.debt = 0;
-    this.damages={"Broken Hull":false,}
-    this.supplies = {Gas:0,Food:0,Medicine:0,Tires:0,Metal:0,Engines:0,Lasers:0,Radio:0, Doctor:0,Martial:0,Hunter:0,Tinker:0,Engineer:0}//Add new items Here
-  }
-}
